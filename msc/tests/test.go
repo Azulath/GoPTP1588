8168e3c82d558119bce864742ae1522ee4d7baf2
azrinyi <alexander.zrinyi@gmail.com>
2014-04-28 22:12:20 +0200

    header continued....

--- msc/tests/test.go
+++ msc/tests/test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"alex/ptp1588boundaryclock/communication"
+	"alex/ptp1588boundaryclock/datasets"
 )
 
 func main() {
@@ -23,6 +25,34 @@ func main() {
 	fmt.Println(third)
 	fmt.Println(fourth)
 	fmt.Println(0xff)
+
+	var b1 [3]byte
+	var b2 [3]byte
+
+	b1[0], b1[1], b1[2] = 0, 1, 2
+	b2[0], b2[1], b2[2] = 3, 4, 5
+
+	fmt.Printf("b1 %+v", b1)
+	fmt.Printf("b2 %+v", b2)
+
+	var msg communication.MessageType
+	msg = communication.Announce
+
+	b1[0] = byte(msg)
+	fmt.Println(msg)
+
+	vers := new(datasets.PortDS)
+	vers.VersionNumber = 128
+
+	b1[1] = vers.VersionNumber
+	fmt.Println(b1)
+
+	zahl := 0
+	zahl += 1
+	fmt.Println(zahl)
+	zahl += (1<<1)
+	zahl += (1<<2)
+	fmt.Println(zahl)
 }
 
 
--- ptp1588boundaryclock/communication/sender.go
+++ ptp1588boundaryclock/communication/sender.go
@@ -3,9 +3,10 @@ package communication
 import (
 	"net"
 	"alex/msc/helper"
+	"alex/ptp1588boundaryclock/datasets"
 )
 
-func MessageSender(ipAddress, port string) {
+func MessageSender(ipAddress, port string, portDS datasets.PortDS, defaultDS datasets.DefaultDS, timePropertiesDS datasets.TimePropertiesDS, messageType MessageType) {
 	address := ipAddress + ":" + port
 
 	udpAddr, err := net.ResolveUDPAddr("udp", address)
@@ -13,27 +14,89 @@ func MessageSender(ipAddress, port string) {
 
 	connection, err := net.DialUDP("udp", nil, udpAddr)
 	helper.ErrHandling(err)
-	handleServer(connection)
+	handleServer(connection, writePTPHeader(portDS, defaultDS, timePropertiesDS, messageType))
 
 }
 
-func handleServer(connection *net.UDPConn) {
+func handleServer(connection *net.UDPConn, header []byte) {
 	//var buffer [512]byte
-	_, err := connection.Write([]byte("writening going on"))
+	//_, err := connection.Write([]byte("writening going on"))
+	_, err := connection.Write(header)
 	helper.ErrHandling(err)
 }
 
-func writePTPHeader(messageType MessageType) []byte {
+func writePTPHeader(portDS datasets.PortDS, defaultDS datasets.DefaultDS, timePropertiesDS datasets.TimePropertiesDS, msgType MessageType) []byte {
 	//TODO transport specific stuff...
-	var header [34]byte
+	header := make([]byte, 34)
 	// transportSpecific AND messageType -> mergen!
-	header[0] = messageType
-	// reserved AND versionPTP (2)
-	header[1] = 2
+	header[0] = byte(msgType)
+	// reserved AND versionPTP (2) -> uint8 => no conversion needed
+	header[1] = portDS.VersionNumber
 	// messageLength uint16
-	header[2], header[3] = 0, 0
-	// domainNumber
-
+	header[2], header[3] = calculateMessageLength()
+	// domainNumber uint8 -> defaultDS.domainNumber
+	header[4] = defaultDS.DomainNumber
+	// reserved -> 0
+	header[5] = 0
+	// flag -> mega many much stuff
+	header[6], header[7] = setHeaderFlagField(defaultDS, timePropertiesDS, msgType)
+	// correction field int64 -> slice
+	header[8] = 0
+	header[15] = 0
+	// reserved
+	header[16], header[17], header[18], header[19] = 0, 0, 0, 0
+	// sourcePortIdentity PortIdentity -> slice
+	header[20] = 0
+	header[29] = 0
+	// sequenceID
+	header[30] = 0
+	header[31] = 0
+	// controlField -> DEPRECATED -> if stuff going on soon
+	header[32] = 0
+	// logMessageInterval -> kummt auf value und so au, mega muh!
+	header[33] = 0
 	return header
+}
+
+// Calculates the length of the message
+// Will be larger than the header (34)
+func calculateMessageLength() (byte, byte) {
+	length := uint16(35)
+	return uint8(length >> 8), uint8(length)
+}
+
+func setHeaderFlagField(defaultDS datasets.DefaultDS, timePropertiesDS datasets.TimePropertiesDS, msgType MessageType) (flagField1 byte, flagField2 byte) {
+	// TODO: i versteh 13.3 Table 20 net
+	if msgType == Announce || msgType == Sync || msgType == Follow_Up || msgType == Delay_Resp {
+		flagField1 += 0
+	}
+
+	if (msgType == Sync || msgType == Pdelay_Resp) && defaultDS.TwoStepFlag {
+		flagField1 += (1<<1)
+	}
+
+	// TODO: if unicast und rest
+
+	if msgType == Announce {
+		if timePropertiesDS.Leap61 {
+			flagField2 += 1
+		}
+		if timePropertiesDS.Leap59 {
+			flagField2 += (1<<1)
+		}
+		if timePropertiesDS.CurrentUtcOffsetValid {
+			flagField2 += (1<<2)
+		}
+		if timePropertiesDS.PtpTimescale {
+			flagField2 += (1<<3)
+		}
+		if timePropertiesDS.TimeTraceable {
+			flagField2 += (1<<4)
+		}
+		if timePropertiesDS.FrequencyTraceable {
+			flagField2 += (1<<5	)
+		}
+	}
+	return
 }
 
